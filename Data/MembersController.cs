273eb6ce4797dce12aec6cde85e6235ee847577b
ArthurChenEvans <arthurchenevans@protonmail.com>
2026-03-03 11:34:37 +0000

    Add basic member controller with get endpoints

--- Data/MembersController.cs
+++ Data/MembersController.cs
@@ -1,10 +1,28 @@
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 [Route("api/[controller]")]
 [ApiController]
-public class MembersController : ControllerBase
+public class MembersController(AppDbContext context) : ControllerBase
 {
-    
+   [HttpGet]
+   public async Task<ActionResult<IReadOnlyList<AppUser>>> Get()
+   {
+      var member = await context.Users.ToListAsync();
+      
+      return member;
+   }
+
+   [HttpGet("{id}")]
+   public async Task<ActionResult<AppUser>> Get(Guid id)
+   {
+      var member = await context.Users.FindAsync(id);
+
+      if (member == null) return NotFound();
+      
+      return member;
+   }
 }
--- obj/Debug/net10.0/API.AssemblyInfo.cs
+++ obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ce6eb38dc73f88cc68e1b53a3e49338b83b506")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed25f3c1b03ccc78ddcb5c5a6b4609805033c933")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
